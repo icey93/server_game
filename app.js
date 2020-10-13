427954cc9bf679d931209005dfd8133c5913880e
YanBing01 <yanbing01@zuoyebang.com>
2020-10-13 21:20:21 +0800

    add entry

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-app.listen(80)
-
-app.get('/', (req, res, next) => {
-  res.end('index')
+const readFile = require('./utils/readFile')
+const indexHtmlPath = '/web/game/server_client/dist/index.html'
+app.get('/page', (req, res, next) => {
+  readFile.readBinary(indexHtmlPath, res)
 })
 
 app.get('/user', (req, res, next)=> {
   res.end('user')
 })
+
+app.listen(8080)
